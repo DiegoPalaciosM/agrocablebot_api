4f708aebd192bdc4464bc53a5b7968a10374c91c
Diego <diegopalmo@hotmail.com>
2024-01-19 20:35:26 -0500

    Modelos

--- agrocablebot/settings.py
+++ agrocablebot/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'sensores',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'agrocablebot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'agrocablebot',
+        'USER': os.environ["MARIADB_USER"],
+        'PASSWORD': os.environ["MARIADB_PASSWORD"],
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
 
@@ -103,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-CO'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
--- api/models.py
+++ api/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+class Acelerometro(models.Model):
+    pitch = models.FloatField()
+    roll = models.FloatField()
+    yaw = models.FloatField()
+    prueba = models.IntegerField()
+    fecha_creacion = models.DateTimeField()
+
+class Giroscopio(models.Model):
+    pitch = models.FloatField()
+    roll = models.FloatField()
+    yaw = models.FloatField()
+    prueba = models.IntegerField()
+    fecha_creacion = models.DateTimeField()
+
+class Magnetometro(models.Model):
+    x = models.FloatField()
+    y = models.FloatField()
+    z = models.FloatField()
+    prueba = models.IntegerField()
+    fecha_creacion = models.DateTimeField()
+
+class Humedad(models.Model):
+    value = models.FloatField()
+    prueba = models.IntegerField()
+    fecha_creacion = models.DateTimeField()
+
+class Presion(models.Model):
+    value = models.FloatField()
+    prueba = models.IntegerField()
+    fecha_creacion = models.DateTimeField()
+
+class Temperatura(models.Model):
+    value = models.FloatField()
+    prueba = models.IntegerField()
+    fecha_creacion = models.DateTimeField()
